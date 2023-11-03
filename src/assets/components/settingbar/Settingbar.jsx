eaf13e29fd1684a5913b072627fcfca1208a9e3c
Mason Yates <Tidalabysstv@gmail.com>
2023-11-03 11:40:49 -0500

    finished the form/settings visual elements

---
+++ src/assets/components/settingbar/Settingbar.jsx
@@ -0,0 +1,12 @@
+import './settings.css';
+
+export default function Settingbar() {
+  return (
+    <>
+        <div className="settingsbar">
+            <button className="formpageaccess">FORM</button>
+            <button className="colorfontsettings">SETTINGS</button>
+        </div>
+    </>
+  )
+}
--- src/assets/layout/Layout.jsx
+++ src/assets/layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/header/Header.jsx';
 import Footer from '../components/footer/Footer.jsx';
 import Sheetrep from '../sheet/Sheetrep.jsx';
+import Settingbar from '../components/settingbar/Settingbar.jsx';
 
 export default function Layout() {
   return (
     <>
         <Header />
-
+        <Settingbar />
+        
         <Sheetrep />
         <Footer />
     </>
